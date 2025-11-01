d02e8442edb07aba46f66914252d0c54f0889b00
ShahenDeveloper <shaheendeveloper8@gmail.com>
2025-11-01 12:03:04 +0600

    Update Inngest client ID and use environment variable
    SIGNING KEY

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,4 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "vibe-development",eventKey: "OWVWNJxA0UQBkn1yt-sRMrzDhGpkHauDm_r8wGvRnEuDGe1V7Ycoo3128S1Ha1VDUiKQzdjMM7lJg1DbzeGxxw" });
+export const inngest = new Inngest({ id: "vered-development123",eventKey: process.env.INNGEST_EVENT_KEY });
